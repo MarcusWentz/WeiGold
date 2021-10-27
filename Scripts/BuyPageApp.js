6cd3111adbc5208ba5b2c9b7fe318ea969423c7b
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-10-26 21:35:05 -0400

    Update BuyPageApp.js

--- Scripts/BuyPageApp.js
+++ Scripts/BuyPageApp.js
@@ -63,13 +63,18 @@ contractDefined_JS.methods.State().call((err, balance) => {
   if( (balance&4) == 4 ) {
     document.getElementById("changeBuyGold").className = "btn btn-outline-danger"
     document.getElementById("getValueWEI_Gold_Price").className = "text-danger"
-    document.getElementById("getValueWEI_Gold_Price").innerHTML = "GOLD SOLD!"
+    document.getElementById("getValueUSD_Gold_Price").className = "text-danger"
+    document.getElementById("getValueWEI_Gold_Price").innerHTML = "GOLD"
+    document.getElementById("getValueUSD_Gold_Price").innerHTML = "SOLD!"
   }
   if( (balance&4) != 4 ){
     document.getElementById("changeBuyGold").className = "btn btn-outline-warning"
     document.getElementById("getValueWEI_Gold_Price").className = "text-warning"
     contractDefined_JS.methods.getLatest_WEI_Gold_Price().call((err, balance) => {
       document.getElementById("getValueWEI_Gold_Price").innerHTML =  (balance/(10**18)).toFixed(6) + " ETH"
+        contractDefined_JS.methods.getLatest_ETH_USD_Price().call((err, convertToETH_USD) => {
+          document.getElementById("getValueUSD_Gold_Price").innerHTML = "$"  + ( (balance*convertToETH_USD)/(10**26) ).toFixed(2) + " USD"
+      })
     })
   }
   if( (balance&2) == 2 ){
@@ -78,7 +83,6 @@ contractDefined_JS.methods.State().call((err, balance) => {
     document.getElementById("getValueUSD_Silver_Price").className = "text-danger"
     document.getElementById("getValueWEI_Silver_Price").innerHTML = "SILVER!"
     document.getElementById("getValueUSD_Silver_Price").innerHTML = "SOLD!"
-
   }
   if( (balance&2) != 2 ){
     document.getElementById("changeBuySilver").className = "btn btn-outline-secondary"
@@ -96,7 +100,6 @@ contractDefined_JS.methods.State().call((err, balance) => {
     document.getElementById("getValueUSD_Oil_Price").className = "text-danger"
     document.getElementById("getValueWEI_Oil_Price").innerHTML = "OIL"
     document.getElementById("getValueUSD_Oil_Price").innerHTML = "SOLD!"
-
   }
   if( (balance&1) != 1 ){
     document.getElementById("changeBuyOil").className = "btn btn-outline-light"
