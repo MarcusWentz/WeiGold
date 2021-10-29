e604544e238b21dd5b4f03eeb072c6f8ab82527d
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-10-29 03:15:26 -0400

    Update and rename WeiGoldV5.sol to WeiGoldV6.sol

--- Contract/WeiGoldV6.sol
+++ Contract/WeiGoldV6.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract WeiGold{
-    
+
     AggregatorV3Interface internal priceFeedETHforUSD;
     AggregatorV3Interface internal priceFeedWEIforGold;
     AggregatorV3Interface internal priceFeedWEIforSilver;
@@ -22,20 +22,20 @@ contract WeiGold{
         priceFeedWEIforSilver = AggregatorV3Interface(0x9c1946428f4f159dB4889aA6B218833f467e1BfD);
         priceFeedWEIforOil = AggregatorV3Interface(0x6292aA9a6650aE14fbf974E5029f36F95a1848Fd);
     }
-    
+
     function getLatest_ETH_USD_Price() public view returns (int) {
         (
             uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound
         ) = priceFeedETHforUSD.latestRoundData();
         return (price);
     }
-    
+
     function getLatest_WEI_Gold_Price() public view returns (uint) {
         (
             uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound
         ) = priceFeedWEIforGold.latestRoundData();
-        return uint( (price*(10**18)*((1000+Scale_Fee)/1000)) / getLatest_ETH_USD_Price() );    
-    } 
+        return uint( (price*(10**18)*((1000+Scale_Fee)/1000)) / getLatest_ETH_USD_Price() );
+    }
     function getLatest_WEI_Silver_Price() public view returns (uint) {
         (
             uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound
@@ -48,49 +48,52 @@ contract WeiGold{
         ) = priceFeedWEIforOil.latestRoundData();
         return uint( (price*(10**18)*((1000+Scale_Fee)/1000)) / getLatest_ETH_USD_Price() );
     }
-    
+
     modifier ContractOwnnerCheck() {
         require(msg.sender == Owner, "Only contract owner (deployer) can access this function.");
         _;
     }
 
-    event contractStateChangeEvent(
-        uint indexed date,
-        address indexed from,
-        uint valueChangeEventWenjs,
-        int feeChange
-    );
     
+    event contractScale_FeeChangeEvent(
+      int feeChange
+    );
+
+    event contractStateChangeEvent(
+        address indexed from,
+        uint valueChangeEventWenjs
+    );
+
     function BuyGold() public payable {
         require((State&4)==0,  "Gold is sold out already!");
         require(getLatest_WEI_Gold_Price() > 0, "Contract is unable to read Chainlink pricefeeds.");
         require(msg.value == getLatest_WEI_Gold_Price(), "MSG.VALUE must be equal to getLatest_WEI_Gold_Price");
         State+=4;
-        emit contractStateChangeEvent(block.number, msg.sender, State, Scale_Fee);
+        emit contractStateChangeEvent(msg.sender, State);
     }
-    
+
     function BuySilver() public payable {
         require((State&2)==0, "Silver is sold out already!");
         require(getLatest_WEI_Silver_Price() > 0, "Contract is unable to read Chainlink pricefeeds.");
         require(msg.value == getLatest_WEI_Silver_Price(), "MSG.VALUE must be equal to getLatest_WEI_Silver_Price()!");
         State+=2;
-        emit contractStateChangeEvent(block.number, msg.sender, State, Scale_Fee);
+        emit contractStateChangeEvent(msg.sender, State);
     }
-        
+
     function BuyOil() public payable {
         require((State&1)==0, "Oil is sold out already!");
         require(getLatest_WEI_Oil_Price() > 0, "Contract is unable to read Chainlink pricefeeds.");
         require(msg.value == getLatest_WEI_Oil_Price(), "MSG.VALUE must be equal to getLatest_WEI_Oil_Price()!");
         State+=1;
-        emit contractStateChangeEvent(block.number, msg.sender, State, Scale_Fee);
+        emit contractStateChangeEvent(msg.sender, State);
     }
 
     function OwnerChangeScaleFee(int update_Scale_Fee) public ContractOwnnerCheck {
         require(Scale_Fee != update_Scale_Fee, "Input value is already the same as Scale_Fee!");
         Scale_Fee = update_Scale_Fee;
-        emit contractStateChangeEvent(block.number, msg.sender, State, Scale_Fee);
+        emit contractScale_FeeChangeEvent(Scale_Fee);
     }
-    
+
     function OwnerChangeStateServoAutoWithdraw(uint update_State) public ContractOwnnerCheck {
         require(State != update_State, "Input value is already the same as State!");
         require(update_State < 8, "Input must be less than 8!!");
@@ -98,7 +101,7 @@ contract WeiGold{
         if(address(this).balance> 0){
             payable(Owner).transfer(address(this).balance);
         }
-        emit contractStateChangeEvent(block.number, msg.sender, State, Scale_Fee);
+        emit contractStateChangeEvent(msg.sender, State);
     }
-    
+
 }
