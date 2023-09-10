0ab85a2d68ee9374cc4bd4fa699eb7d77e9b1f76
MarcusWentz <markzcollege404@gmail.com>
2023-09-09 22:40:26 -0400

    Stage Foundry

--- src/WeiGold.sol
+++ src/WeiGold.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // forge install @chainlink/contracts
 
 error slotEmpty();
 error oraclePriceFeedZero();
