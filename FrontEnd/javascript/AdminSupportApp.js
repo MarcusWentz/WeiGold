e957474dfe371b8bffb1663f5036366e7e281257
MarcusWentz <markzcollege404@gmail.com>
2024-09-05 00:34:44 -0400

    Optimism Sepolia Admin Page

--- FrontEnd/javascript/AdminSupportApp.js
+++ FrontEnd/javascript/AdminSupportApp.js
@@ -5,7 +5,7 @@
 let accounts = [];
 document.getElementById("enableEthereumButton").innerHTML =  "Connect Metamask 🦊"
 
-const sepoliaChainId = 11155111
+const optimismSepoliaChainId = 11155420
 
 //If Metamask is not detected the user will be told to install Metamask.
 function detectMetamaskInstalled(){
@@ -31,7 +31,7 @@ detectMetamaskInstalled()
 const ethereumButton = document.querySelector('#enableEthereumButton');
 ethereumButton.addEventListener('click', () => {
     detectMetamaskInstalled()
-    enableMetamaskOnSepolia()
+    enableMetamaskOnOptimismSepolia()
 });
 
 async function getAccount() {
@@ -43,8 +43,8 @@ async function getAccount() {
 const provider = new ethers.providers.Web3Provider(window.ethereum); //Imported ethers from index.html with "<script src="https://cdn.ethers.io/lib/ethers-5.6.umd.min.js" type="text/javascript"></script>".
 
 //Now build the contract with Web3.
-const contractAddress_JS = '0xf72ad88C2DB9e89738773Db2388770572AD3D628'
-const contractABI_JS = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"etherNotSent","type":"error"},{"inputs":[],"name":"msgValueTooSmall","type":"error"},{"inputs":[],"name":"notOwner","type":"error"},{"inputs":[],"name":"oraclePriceFeedZero","type":"error"},{"inputs":[],"name":"slotEmpty","type":"error"},{"anonymous":false,"inputs":[],"name":"slotsUpdated","type":"event"},{"inputs":[{"internalType":"uint256","name":"slot","type":"uint256"}],"name":"BuyGold","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"slot","type":"uint256"},{"internalType":"uint256","name":"count","type":"uint256"}],"name":"OwnerUpdateSlots","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getLatestEthUsdPrice","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLatestWeiGoldPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"scaleFee","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"vendingSlotCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+const contractAddress_JS = '0x090b750b9B5251828E16360Fd69100dc4c674e71'
+const contractABI_JS = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"etherNotSent","type":"error"},{"inputs":[],"name":"msgValueTooSmall","type":"error"},{"inputs":[],"name":"notOwner","type":"error"},{"inputs":[],"name":"oraclePriceFeedZero","type":"error"},{"inputs":[],"name":"slotEmpty","type":"error"},{"anonymous":false,"inputs":[],"name":"slotsUpdated","type":"event"},{"inputs":[{"internalType":"uint256","name":"slot","type":"uint256"}],"name":"BuyGold","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"MAX_BPS","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OWNER","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"slot","type":"uint256"},{"internalType":"uint256","name":"count","type":"uint256"}],"name":"OwnerUpdateSlots","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"SCALE_FEE","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLatestEthUsdPrice","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getLatestWeiGoldPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"vendingSlotCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
 const contractDefined_JS = new ethers.Contract(contractAddress_JS, contractABI_JS, provider);
 
@@ -61,7 +61,7 @@ changeStateInContractEvent.addEventListener('click', () => {
 
 async function ownerChangeStateTx(setValueStateSmartContract) {
 
-  const contractOwner = await contractDefined_JS.owner();
+  const contractOwner = await contractDefined_JS.OWNER();
   // console.log(accounts[0])
   // console.log(contractOwner.toLowerCase())
   if(accounts[0] != contractOwner.toLowerCase()){
@@ -91,30 +91,38 @@ async function ownerChangeStateTx(setValueStateSmartContract) {
 async function getContractValues() {
   let getVendingSlotCountZero = await contractDefined_JS.vendingSlotCount(0)
   if(getVendingSlotCountZero === undefined){
-    document.getElementById("storageSlotZeroCount").innerHTML =  "Install Metamask and select Sepolia Testnet to have a Web3 provider to read blockchain data."
+    document.getElementById("storageSlotZeroCount").innerHTML =  "Install Metamask and select Optimism Sepolia Testnet to have a Web3 provider to read blockchain data."
   }
   else{
     document.getElementById("storageSlotZeroCount").innerHTML = getVendingSlotCountZero
   }
 }
 
- async function enableMetamaskOnSepolia() {
+ async function enableMetamaskOnOptimismSepolia() {
   //Get account details from Metamask wallet.
   getAccount();
-  //Check if user is on the Sepolia testnet. If not, alert them to change to Sepolia.
-  if(window.ethereum.networkVersion != sepoliaChainId){
-    // alert("You are not on the Sepolia Testnet! Please switch to Sepolia and refresh page.")
+
+  // Updated chainId request method suggested by Metamask.
+  let chainIdConnected = await window.ethereum.request({method: 'net_version'});
+
+  // // Outdated chainId request method which might get deprecated:
+  // //  https://github.com/MetaMask/metamask-improvement-proposals/discussions/23
+  // let chainIdConnected = window.ethereum.networkVersion;
+
+  //Check if user is on the Optimism Sepolia testnet. If not, alert them to change to Optimism Sepolia.
+  if(chainIdConnected != optimismSepoliaChainId){
+    // alert("You are not on the Optimism Sepolia Testnet! Please switch to Optimism Sepolia and refresh page.")
     try{
       await window.ethereum.request({
           method: "wallet_switchEthereumChain",
           params: [{
-             chainId: "0xaa36a7"
+             chainId: "0xaa37dc"
           }]
         })
       location.reload(); 
       // alert("Failed to add the network at chainId " + sepoliaChainId + " with wallet_addEthereumChain request. Add the network with https://chainlist.org/ or do it manually. Error log: " + error.message)
     } catch (error) {
-      alert("Failed to add the network at chainId " + sepoliaChainId + " with wallet_addEthereumChain request. Add the network with https://chainlist.org/ or do it manually. Error log: " + error.message)
+      alert("Failed to add the network at chainId " + optimismSepoliaChainId + " with wallet_addEthereumChain request. Add the network with https://chainlist.org/ or do it manually. Error log: " + error.message)
     }
   }
 }
